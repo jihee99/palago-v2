13467e9f7eb949a994a19db5e99a592c21316f19
jihee99 <oosoojh31@gmail.com>
2024-02-13 22:23:43 +0900

    Initial commit

---
+++ src/test/java/com/ex/palago/PalagoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.palago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalagoV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
